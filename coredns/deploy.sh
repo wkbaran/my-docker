f841cf39eab9040f88410cd062fa9ad6bb4b8a50
Bill Baran <bill.baran@gmail.com>
2025-01-18 23:38:00 -0700

    Improve caching. Added postgres and qdrant.

--- coredns/deploy.sh
+++ coredns/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ssh core@nuc1 mkdir -p coredns
-scp -r Corefile core@nuc1:coredns
-docker -H nuc1:2375 compose up -d
+ssh core@nuc1.home mkdir -p coredns
+scp -r Corefile core@nuc1.home:coredns
+docker -H nuc1.home:2375 compose up -d
--- traefik/deploy.sh
+++ traefik/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ssh core@nuc1 mkdir -p traefik/config
-scp -r traefik.yml core@nuc1:traefik
-docker -H nuc1:2375 compose up -d
+ssh core@nuc1.home mkdir -p traefik/config
+scp -r traefik.yml core@nuc1.home:traefik
+docker -H nuc1.home:2375 compose up -d
