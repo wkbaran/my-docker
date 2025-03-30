4b170510532fa8766be844406f0f801ae1498b03
Bill Baran <bill.baran@gmail.com>
2025-03-29 21:28:44 -0600

    Homepage background image

--- homepage/deploy.sh
+++ homepage/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-ssh core@nuc1.home mkdir -p homepage/config homepage/public/icons
+ssh core@nuc1.home mkdir -p homepage/config homepage/public/icons homepage/public/images
 scp config/settings.yaml config/services.yaml config/widgets.yaml config/bookmarks.yaml core@nuc1.home:homepage/config
 scp public/icons/* core@nuc1.home:homepage/public/icons
+scp public/images/* core@nuc1.home:homepage/public/images
 docker -H nuc1.home:2375 compose up -d
