daec5b3ba66f7e4ccf2e6f99a5e46973ff0c7c0d
Bill Baran <bill.baran@gmail.com>
2025-03-31 22:17:30 -0600

    Still fixing timescaling

--- timescale/deploy.sh
+++ timescale/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ssh core@nuc1.home mkdir -p time
-scp -r init-data.sh core@nuc1.home:time
+ssh core@nuc1.home mkdir -p timescale
+scp -r init-data.sh postgresql.conf core@nuc1.home:timescale
 docker -H nuc1.home:2375 compose up -d
